daa87c6dbb446367ecf0a1a39763fdfc541460cb
hamidem <33391305+hamidem@users.noreply.github.com>
2019-06-28 18:33:37 +0200

    Add files via upload

---
+++ NewsApp-1/NewsApp-1/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NewsApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Angular Routing",
+                "{*url}",
+                new { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
+        }
+
+    }
+}
